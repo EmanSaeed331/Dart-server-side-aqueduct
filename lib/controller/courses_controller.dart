0fc0a39ec36a8b49f2aa137d4a9bbe68f4a2cc35
EmanSaeed331 <emansaeed5330@gmail.com>
2021-11-01 03:39:20 +0200

    resourse controller & Operation methods & anotation methods

--- lib/controller/courses_controller.dart
+++ lib/controller/courses_controller.dart
@@ -1,29 +1,25 @@
 import 'package:project3/project3.dart';
 
-class CoursesController extends Controller {
+class CoursesController extends ResourceController {
   final List _courses = [
     {'id': 1, 'name': 'Dart Programming'},
     {'id': 2, 'name': 'Flutter'},
     {'id': 3, 'name': 'Java'},
   ];
 
-  @override
-  FutureOr<RequestOrResponse> handle(Request request) {
-    switch (request.method) {
-      case 'GET':
-        return Response.ok(_courses);
-        break;
-      case 'POST':
-        return Response.ok('POST Method');
-        break;
-      case 'PUT':
-        return Response.ok('PUT Method');
-        break;
-      case 'DELETE':
-        return Response.ok('DELETE Method');
-        break;
-      default:
-        return Response.ok('UNDEFIEND');
+  @Operation.get()
+  Future<Response> getAllCourses() async {
+    return Response.ok(_courses);
+  }
+
+  @Operation.get('id')
+  Future<Response> getCourseID() async {
+    final id = int.parse(request.path.variables['id']);
+
+    final course = _courses.firstWhere((element) => element['id'] == id,
+        orElse: () => null);
+    if (course == null) {
+      return Response.ok(course);
     }
   }
 }
